a2c453b7b9a5d42e347edecbf53f7a5a1949bc52
ohmic <md3lgado@gmail.com>
2016-04-10 01:09:44 -0400

    new file:   Translator.py

---
+++ Translator.py
@@ -0,0 +1,23 @@
+import sys
+from http import client
+##variables for the search query
+knownWord = ''
+knownLanguage = ''
+translatingLanguage = ''
+
+if len(sys.argv) is not 4:
+ print('error in arguments, format program as python translater.py knownWord knownLanguage translatingLanguage')
+else:
+ conn = client.HTTPConnection("www.linguee.com") #connect to their server
+ conn.request("GET", "/"+str(sys.argv[2])+"-"+str(sys.argv[3])+"/search?query="+str(sys.argv[1])) #linguee is done entirely through the web with results based on the client's browser
+ r1 = conn.getresponse() #get the response(raw html)
+ stringData = str(r1.read()) #turn the response into a string for analysis
+ tagIndex = 0 #loop control variable
+ while tagIndex < len(stringData): #search through the entire file
+  tagIndex = stringData.find('dictLink featured', tagIndex) #dictLink featured is the tag right before the translated term. In case they change their website this argument will have to be changed. Luckily it only appears once
+  if tagIndex == -1: #end of file, get out
+   break
+  SpecificTagCloseIndex = stringData.find('>', tagIndex)+1 #find the closing tag after dickLink featured, this ends the entire tag and is where the translation begins
+  TagAfterSpecificTagOpenIndex = stringData.find('<', SpecificTagCloseIndex) #find the opening tag of the next element, this is where the translation ends
+  print(stringData[SpecificTagCloseIndex:TagAfterSpecificTagOpenIndex]) #print out what's between the tags, the translation
+  tagIndex += 2 #iterate so we don't have an infinite loop
